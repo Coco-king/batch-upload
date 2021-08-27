8fdd9206afd5ae45a994079b161858688f743540
ex_wanggang1 <ex_wanggang1@axatp.com>
2021-08-27 09:13:47 +0800

    修改测试时间类的时间格式化为线程安全的

--- src/main/java/top/codecrab/upload/utils/Times.java
+++ src/main/java/top/codecrab/upload/utils/Times.java
@@ -1,13 +1,14 @@
 package top.codecrab.upload.utils;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 测试耗时
  */
 public class Times {
-    private static final SimpleDateFormat FMT = new SimpleDateFormat("HH:mm:ss.SSS");
+
+    private static final DateTimeFormatter FMT = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
 
     public interface Task {
         void execute();
@@ -17,11 +18,11 @@ public class Times {
         if (task == null) return;
         title = (title == null) ? "" : ("【" + title + "】");
         System.out.println(title);
-        System.out.println("开始：" + FMT.format(new Date()));
+        System.out.println("开始：" + FMT.format(LocalTime.now()));
         long begin = System.currentTimeMillis();
         task.execute();
         long end = System.currentTimeMillis();
-        System.out.println("结束：" + FMT.format(new Date()));
+        System.out.println("结束：" + FMT.format(LocalTime.now()));
         double delta = (end - begin) / 1000.0;
         System.out.println("耗时：" + delta + "秒");
         System.out.println("-------------------------------------");
